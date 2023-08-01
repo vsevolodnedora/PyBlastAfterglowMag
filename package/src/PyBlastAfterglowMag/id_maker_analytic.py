[dev:skymap_a] tophat; adaptive; scaling fixed; problem with theta_max

package/src/PyBlastAfterglowMag/id_maker_analytic.py
@@ -246,10 +246,10 @@ class JetStruct:
         self.dist_s_pw = np.zeros( self.nlayers_pw )
         self.setThetaGridPW()
 
-        one_min_cos = 2 * np.sin(0.5 * theta_h) * np.sin(0.5 * theta_h)
-        ang_size_layer = 2.0 * np.pi * one_min_cos
+        one_min_cos = 2. * np.sin(0.5 * theta_h) * np.sin(0.5 * theta_h)
+        ang_size_layer = 2.0 * np.pi * one_min_cos / (4.0 * np.pi)
         for i in range(len(self.cthetas0)):
-            self.dist_E0_pw[i] = E_iso * ang_size_layer / (4.0 * np.pi)
+            self.dist_E0_pw[i] = E_iso * ang_size_layer
             self.dist_Mom0_pw[i] = MomFromGam(Gamma0)
             # Gamma = GamFromMom(self.dist_Mom0_pw[i])
             # self.dist_M0_pw[i] = M0 < 0 ? dist_E0_pw[i] / (Gamma * CGS::c * CGS::c) : M0 * ang_size_layer / (4 * np.pi);

package/src/PyBlastAfterglowMag/id_maker_from_kenta_bns.py
@@ -21,6 +21,7 @@ import re
 import os
 import sys
 import argparse
+from matplotlib.colors import LogNorm, Normalize
 
 from .id_maker_tools import (reinterpolate_hist, reinterpolate_hist2, compute_ek_corr)
 from .utils import (cgs, get_Gamma, get_Beta, find_nearest_index, MomFromGam, GamFromMom, BetFromMom)
@@ -276,13 +277,14 @@ def get_ej_data_for_text(files : list[str],
     else:
         return (sorted_pars_list, sorted_vals)
 
+
 def prepare_kn_ej_id_2d(files : list[str],
                         outfpaths : list[str],
                         dist="pw",
                         req_times=np.array([25]),
                         new_theta_len=None,
                         new_vinf_len=None,
-                        r0type="fromrho", t0=100, r0frac=0.5,
+                        r0type="fromrho", t0=.1, r0frac=0.5,
                         verbose = True,
                         ):
 
@@ -307,6 +309,51 @@ def prepare_kn_ej_id_2d(files : list[str],
         ye_corr = pars["ye"].T
         rho_corr = pars["rho"].T
 
+        if (r0type == "fromrho"):
+            r = np.zeros_like(mass_corr)
+            for ith in range(len(theta_corr2)):
+                r_base = t0 * cgs.c * vinf_corr2[0]
+                r[0,ith] = r_base
+                for ir in range(1,len(vinf_corr2)):
+                    if (mass_corr[ir,ith]==0. or rho_corr[ir,ith]==0.):
+                        print(f"Error ir={ir} ith={ith} mass={mass_corr[ir,ith]} rho={rho_corr[ir,ith]}. Setting mass to 0.")
+                        mass_corr[ir,ith]=0.
+                        continue
+                    r_i = (3./4.) * (1./np.pi) * len(theta_corr2) * mass_corr[ir,ith] / rho_corr[ir,ith] + r[ir-1,ith]**3
+                    r_i = r_i**(1./3.)
+                    r[ir,ith] = r_i
+                    if ((r_i <= r[ir-1,ith]) or (~np.isfinite(r[ir,ith]))):
+                        raise ValueError()
+        elif (r0type == "frombeta"):
+            r = np.zeros_like(mass_corr)
+            t = t0
+            for ith in range(len(theta_corr2)):
+                for ir in range(len(vinf_corr2)):
+                    r[ir,ith] = BetFromMom(vinf_corr2[ir])*cgs.c * t # TODO THis is theta independent!
+        else:
+            raise KeyError(f"r0type={r0type} is not recognized")
+
+
+        # fig, axes = plt.subplots(ncols=1, nrows=3, figsize=(5,9),sharex='all')
+        # im=axes[0].pcolormesh(theta_corr2* 180 / np.pi,vinf_corr2,mass_corr,norm=LogNorm(mass_corr[mass_corr>0].max()*1e-3,mass_corr[mass_corr>0].max()))
+        # axes[0].set_title("Mass")
+        # axes[0].set_ylabel("beta")
+        # # axes[0].set_xticklabels(["{:.1f}".format(val) for val in theta_corr2 * 180 / np.pi])
+        # # axes[0].set_yticklabels(["{:.2f}".format(val) for val in vinf_corr2])
+        # # axes[0].set_yscale('log')
+        # plt.colorbar(im)
+        # im=axes[1].pcolormesh(theta_corr2* 180 / np.pi,vinf_corr2,rho_corr,norm=LogNorm(rho_corr[rho_corr>0].max()*1e-3,rho_corr[rho_corr>0].max()))
+        # axes[1].set_title("rho")
+        # axes[1].set_ylabel("beta")
+        # plt.colorbar(im)
+        # im=axes[2].pcolormesh(theta_corr2* 180 / np.pi,vinf_corr2,r,norm=LogNorm(r[r>0].max()*1e-3,r[r>0].max()))
+        # axes[2].set_title("r")
+        # axes[2].set_ylabel("beta")
+        # axes[2].set_xlabel("theta")
+        # plt.colorbar(im)
+        # plt.show()
+
+
         ctheta_corr3 = np.zeros_like(ek_corr2)
         theta_corr3 = np.zeros_like(ek_corr2)
         for imom in range(len(vinf_corr2)):
@@ -316,28 +363,42 @@ def prepare_kn_ej_id_2d(files : list[str],
         for ith in range(len(theta_corr2)):
             mom_corr3[:,ith]=np.array( vinf_corr2*get_Gamma(vinf_corr2))
 
-        if (r0type == "fromrho"):
-            r = np.zeros_like(mass_corr)
-            for ith in range(len(ctheta_corr3[0,:])):
-                idx = 0
-                k = r0frac#0.5
-                r[idx,ith] = (k*(3/4./np.pi)*rho_corr[idx,ith]*mass_corr[idx,ith])**(1./3.)
+        # if (r0type == "fromrho"):
+        #     r = np.zeros_like(mass_corr)
+        #     for ith in range(len(ctheta_corr3[0,:])):
+        #         idx = 0
+        #         k = r0frac#0.5
+        #         r[idx,ith] = (k*(3/4./np.pi)*rho_corr[idx,ith]*mass_corr[idx,ith])**(1./3.)
+        #
+        #         if (r[idx,ith] == 0):
+        #             raise ValueError()
+        #         for ir in range(1,len(mom_corr3[:,0]),1):
+        #             _val = (3./4./np.pi)*rho_corr[ir,ith]*mass_corr[ir,ith]
+        #             if (_val < 0):
+        #                 raise ValueError(f"val={_val}")
+        #             _rm1 = r[ir-1,ith]**3
+        #             if(mass_corr[ir,ith]>0):
+        #                 r[ir,ith] = (_rm1 + _val)**(1./3.)
+        #             if ((r[ir-1,ith]>r[ir,ith])and(r[ir,ith]>0)):
+        #                 raise ValueError(f"ir={ir} r[ir-1,ith]={r[ir-1,ith]} r[ir,ith]={r[ir,ith]}")
+        # elif (r0type == "frombeta"):
+        #     r = np.zeros_like(mass_corr)
+        #     t = t0
+        #     for ith in range(len(ctheta_corr3[0,:])):
+        #         for ir in range(0,len(mom_corr3[:,0]),1):
+        #             r[ir,ith] =  BetFromMom(mom_corr3[ir,ith])*cgs.c * t
+        # else:
+        #     raise KeyError(f"r0type={r0type} is not recognized")
 
-                if (r[idx,ith] == 0):
-                    raise ValueError()
-                for ir in range(1,len(mom_corr3[:,0]),1):
-                    _val = (3./4./np.pi)*rho_corr[ir,ith]*mass_corr[ir,ith]
-                    _rm1 = r[ir-1,ith]**3
-                    if(mass_corr[ir,ith]>0):
-                        r[ir,ith] = (_rm1 + _val)**(1./3.)
-        elif (r0type == "frombeta"):
-            r = np.zeros_like(mass_corr)
-            t = t0
-            for ith in range(len(ctheta_corr3[0,:])):
-                for ir in range(0,len(mom_corr3[:,0]),1):
-                    r[ir,ith] =  BetFromMom(mom_corr3[ir,ith])*cgs.c * t
-        else:
-            raise KeyError(f"r0type={r0type} is not recognized")
+        # check that radii are ordered
+
+        # for i in range(len(ctheta_corr3[0,:])):
+        #     for j in range(len(r[:,0])-1):
+        #         if ((r[j+1,i] > 0) and (not (r[j+1,i] > r[j,i]))):
+        #             print (f"i={i} j={j} and r[j+1,i]={r[j+1,i]} and r[j,i]={r[j,i]}")
+        #             print(r)
+        #             exit(1)
+                # assert r[j+1,i] > r[j,i]
 
         # self.o_pba.setEjectaStructNumeric(theta_corr2, vinf_corr2, ek_corr2, fac, True, self.pars_kn["eats_method"])
 

package/src/PyBlastAfterglowMag/interface.py
@@ -235,7 +235,7 @@ class Base:
                                           sep1="# ---------------------- GRB afterglow ----------------------",
                                           sep2="# --------------------------- END ---------------------------")
         if "fname_dyn" in grb_opts.keys(): self.fpath_dyn = self.res_dir + grb_opts["fname_dyn"]
-        if "fname_spec" in grb_opts.keys(): self.fpath_spec = self.res_dir + grb_opts["fname_spec"]
+        if "fname_spectrum" in grb_opts.keys(): self.fpath_spec = self.res_dir + grb_opts["fname_spectrum"]
         if "fname_light_curve" in grb_opts.keys(): self.fpath_light_curve = self.res_dir + grb_opts["fname_light_curve"]
         if "fname_sky_map" in grb_opts.keys(): self.fpath_sky_map = self.res_dir + grb_opts["fname_sky_map"]
         return (grb_pars,grb_opts)
@@ -437,7 +437,12 @@ def combine_images(xs, ys, datas, verbose=False, hist_or_int="int", shells=False
             edges_y = np.mgrid[ys.min()*extend:ys.max()*extend:ny]
             # grid_x, grid_y = np.mgrid[xrs_i.min() * 1.2:xrs_i.max() * 1.2:nx,
             #                  yrs_i.min() * 1.2:yrs_i.max() * 1.2:ny]
-            i_zz, _ = np.histogramdd(tuple([xs, ys]), bins=tuple([edges_x, edges_y]), weights=datas)
+
+            xs = xs[datas > 0]
+            ys = ys[datas > 0]
+            datas = datas[datas>0]
+
+            i_zz, _ = np.histogramdd(tuple([xs, ys]), bins=tuple([edges_x, edges_y]), weights=datas,normed='density')
             grid_x = 0.5 * (edges_x[1:] + edges_x[:-1])
             grid_y = 0.5 * (edges_y[1:] + edges_y[:-1])
             print(i_zz.shape)
@@ -849,6 +854,8 @@ def get_skymap_lat_dist(all_x, all_y, all_fluxes, collapse_axis="y", fac=1.0, nx
     # edges_x = np.mgrid[all_x.min() * extend:all_y.max() * extend:nx]
     # edges_y = np.mgrid[all_x.min() * extend:all_y.max() * extend:ny]
 
+    if (np.sum(all_x)==0. or np.sum(all_y)==0):
+        raise ValueError(" x_arr or y_arr arrays in the image all FULL 0. Cannot re-interpolate!")
     image = interpolate.griddata(np.array([all_x, all_y]).T * fac, all_fluxes,
                                  (grid_x * fac, grid_y * fac), method='linear', fill_value=0)
     latAvDist, latAvDist2, latMaxDist = lateral_distributions(grid_x, grid_y, image, collapse_axis=collapse_axis)
@@ -1022,13 +1029,13 @@ class Ejecta(Base):
     # ejecta spectrum
     def _check_if_loaded_spec(self):
         if (self.fpath_spec is None):
-            raise IOError("self.fpath_kn_spec is not set")
+            raise IOError("self.fpath_spec is not set")
         if (self.spec_dfile is None):
             self.spec_dfile = h5py.File(self.fpath_spec)
     # ejecta lightcurves
     def _check_if_loaded_lc(self):
         if (self.fpath_light_curve is None):
-            raise IOError("self.fpath_kn_light_curve is not set")
+            raise IOError("self.fpath_light_curve is not set")
         if (self.lc_dfile is None):
             self.lc_dfile = h5py.File(self.fpath_light_curve)
     # ejecta skymaps
@@ -1060,22 +1067,28 @@ class Ejecta(Base):
             for il in range(nlayers):
                 arr.append(self.get_dyn_arr(v_n, ishell=ishell, ilayer=il))
             arr = np.reshape(np.array(arr), newshape=(nlayers, len(arr[0])))
+            if (np.sum(arr) == 0):
+                print(f"Warning np.sum(arr)=0 for ishell={ishell} ilayer={ilayer}")
             return arr
         elif ((ishell is None) and (not ilayer is None)):
             arr = []
             for ish in range(nshells):
                 arr.append(self.get_dyn_arr(v_n, ishell=ish, ilayer=ilayer))
             arr = np.reshape(np.array(arr), newshape=(nshells, len(arr[0])))
+            if (np.sum(arr) == 0):
+                print(f"Warning np.sum(arr)=0 for ishell={ishell} ilayer={ilayer}")
             return arr
         elif ((not ishell is None) and (not ilayer is None)):
             layer = "shell={} layer={} key={}".format(ishell, ilayer, v_n)
             if (not layer in list(dfile.keys())):
-                raise NameError("Layer {} (aka '{}') is not in the ejecta dyn. file.\n Available: {}"
-                                .format(ilayer, layer, dfile.keys()))
+                raise NameError(f"Layer {ilayer} (key '{layer}') is not in the ejecta dyn. file nlayer={nlayers} nshells={nshells}")
             # if (not v_n in dfile[layer].keys()):
             #     raise NameError("v_n {} is not in the ejecta dyn. dfile[{}].keys() \n Avaialble: {}"
             #                     .format(v_n, layer, dfile[layer].keys()))
-            return np.array(dfile[layer])
+            arr = np.array(dfile[layer])
+            if (np.sum(arr) == 0):
+                print(f"Warning np.sum(arr)=0 for ishell={ishell} ilayer={ilayer}")
+            return arr
         else:
             raise NameError()
 
@@ -1127,7 +1140,8 @@ class Ejecta(Base):
         # nshells = int(dfile.attrs["nshells"])
         utimes = self.get_lc_times(spec=spec,unique=True)
         ufreqs = self.get_lc_freqs(spec=spec,unique=True)
-        fluxes = np.array(dfile["total_fluxes"])
+        print(dfile.keys())
+        fluxes =  np.array(dfile["total_power"]) if spec else np.array(dfile["total_fluxes"])
 
         # key = str("totalflux at freq={:.4e}".format(3e9)).replace('.', ',')
         # key = str("totalflux at freq={:.4e}".format(3e9))
@@ -1253,8 +1267,8 @@ class Ejecta(Base):
                 arr = np.array(dfile["shell={} layer={}".format(ishell, ilayer)])
                 if (time is None):
                     data = np.vstack((
-                        [self.get_lc(freq=_freq,ishell= None,ilayer=None,spec=True)[1:-1]
-                            for _freq in self.get_lc_freqs(spec=True,unique=True)]
+                        [self.get_lc(freq=_freq,ishell=ishell,ilayer=ilayer,spec=spec)[1:-1]
+                            for _freq in self.get_lc_freqs(spec=spec,unique=True)]
                     ))
                     return data
 
@@ -1306,6 +1320,8 @@ class Ejecta(Base):
                 arr = np.array(dfile["shell={} layer={}".format(ishell, ilayer)])
                 arr = arr[np.where(freqs==_freq)]
                 fluxes1d = arr  # [freq,time]
+                if (len(fluxes1d)!=len(utimes)):
+                    raise ValueError("size mismatch")
                 return fluxes1d
             else:
                 raise NameError()
@@ -1456,7 +1472,10 @@ class Ejecta(Base):
                 arr = np.array(self.skymap_dfile["totalflux at freq={:.4e}".format(freq)])
                 if (len(arr)==1):
                     return arr[0]
-                assert self.get_skymap_times().min() < time < self.get_skymap_times().max()
+                if (time < self.get_skymap_times().min()):
+                    raise ValueError(f"time {time} < get_skymap_times().min()={self.get_skymap_times().min()}")
+                if (time > self.get_skymap_times().max()):
+                    raise ValueError(f"time {time} > get_skymap_times().max()={self.get_skymap_times().max()}")
                 # self.get_skymap_totfluxes(freq=freq, shell=None, time=None)
                 val = arr[find_nearest_index(self.get_skymap_times(), time)]
                 return val
@@ -1527,7 +1546,7 @@ class Ejecta(Base):
         return (int_x, int_y, int_zz)
 
 
-    def get_skymap(self, time=None, freq=None, ishell=None, verbose=False, remove_mu=False, renormalize=True):
+    def get_skymap(self, time=None, freq=None, ishell=None, verbose=False, remove_mu=False, renormalize=True, normtype="pw"):
 
         # nx = 200
         # ny = 100
@@ -1642,7 +1661,9 @@ class Ejecta(Base):
                     mu_i = np.array(ddfile["mu"][ish])
                     xrs_i = np.array(ddfile["xrs"][ish]) * cgs.rad2mas / d_l  # m -> mas
                     yrs_i = np.array(ddfile["yrs"][ish]) * cgs.rad2mas / d_l  # m -> mas
-                    # rs_i = np.array(ddfile["r"][ish])
+                    rs_i = np.array(ddfile["r"][ish])
+                    cthetas_i = np.array(ddfile["ctheta"][ish])
+                    cphis_i = np.array(ddfile["cphi"][ish])
                     int_i = np.array(ddfile["intensity"][ish]) * ( d_l ** 2 / cgs.rad2mas ** 2 )  # * dfile.attrs["d_L"] ** 2
                     if remove_mu:
                         # idx1 = abs(mu_i) < min_mu # TODO this is overritten!
@@ -1669,11 +1690,20 @@ class Ejecta(Base):
                         # print(theta_i[np.abs(mu_i) < 1e-5])
                         # print(phi_i[np.abs(mu_i) < 1e-5])
                         # print(np.abs(mu_i).min())
+                        # plt.figure(figsize=(4.6, 3.2))
+                        # plt.semilogy(mu_i, int_i / int_i.max(), '.')
+                        # # plt.semilogy(mu_i, int_i / int_i.max(), 'x', color='red')
+                        # # plt.semilogy(mu_i,int_i2/int_i2.max(), 'o', color='red')
+                        # plt.xlabel(
+                        #     r"$\mu=\sin(\theta_{\rm obs}) \sin(\theta) \sin(\phi) + \cos(\theta_{\rm obs}) \cos(\theta)$")
+                        # plt.ylabel(r"$I/I_{\rm max}$")
+                        # plt.tight_layout()
+                        # plt.show()
 
                         # int_i[mu_i < 1e-3] = 0.
                         int_i *= abs( mu_i)  # TODO I was produced as F / (R^2 abs(mu)), where abs(mu)->0 and I->inf. Problem!!!
                         # * np.sqrt(1 - mu_i**2)#/ ((1-mu_i)*(1+mu_i))
-                        import matplotlib.pyplot as plt
+                        # import matplotlib.pyplot as plt
                         # plt.figure(figsize=(4.6,3.2))
                         # plt.semilogy(mu_i,int_i/int_i.max(), '.')
                         # if len(mu_i[idx]) > 0: plt.semilogy(mu_i[idx],int_i[idx]/int_i[idx].max(), 'x', color='red')
@@ -1687,15 +1717,15 @@ class Ejecta(Base):
                     all_yrs.append(yrs_i)
                     all_zz.append(int_i)
 
-                    # import matplotlib.pyplot as plt
-                    # from matplotlib.colors import Normalize, LogNorm
-                    # from matplotlib import cm, rc, rcParams
-                    # import matplotlib.colors as colors
-                    # from mpl_toolkits.axes_grid1 import make_axes_locatable
-                    # from mpl_toolkits.axes_grid1 import ImageGrid
-                    # from mpl_toolkits.axisartist.grid_finder import MaxNLocator
-                    # from matplotlib.colors import BoundaryNorm
-                    # from matplotlib.ticker import MaxNLocator
+                    import matplotlib.pyplot as plt
+                    from matplotlib.colors import Normalize, LogNorm
+                    from matplotlib import cm, rc, rcParams
+                    import matplotlib.colors as colors
+                    from mpl_toolkits.axes_grid1 import make_axes_locatable
+                    from mpl_toolkits.axes_grid1 import ImageGrid
+                    from mpl_toolkits.axisartist.grid_finder import MaxNLocator
+                    from matplotlib.colors import BoundaryNorm
+                    from matplotlib.ticker import MaxNLocator
                     # plt.close()
                     # layers = int(self.get_dyn_obj().attrs["nlayers"])-1
                     # plt.semilogx(self.get_dyn_arr("tt", ilayer=0) / cgs.day, self.get_dyn_arr("ctheta", ilayer=0),
@@ -1710,20 +1740,25 @@ class Ejecta(Base):
                     # plt.legend()
                     # plt.show()
 
+                    # plt.close()
                     # fig = plt.figure()
                     # cmap = cm.get_cmap('inferno')
                     # my_norm = LogNorm(int_i.max() * 1e-1, int_i.max())
                     # ax = fig.add_subplot(projection='3d')
                     # # ax.scatter(xrs_i.flatten(), yrs_i.flatten(), ((theta_i-theta0_i)*180/np.pi).flatten(),  c=cmap(my_norm(int_i.flatten())))
-                    # ax.scatter(xrs_i.flatten(), yrs_i.flatten(), np.log10(rs_i).flatten(), c=cmap(my_norm(int_i.flatten())))
+                    # # ax.scatter(xrs_i.flatten(), yrs_i.flatten(), np.log10(rs_i).flatten(), c=cmap(my_norm(int_i.flatten())))
                     # # ax.scatter(xrs_i.flatten(), yrs_i.flatten(),mu_i.flatten(),  c=cmap(my_norm(int_i.flatten())))
+                    # # ax.scatter(xrs_i.flatten(), yrs_i.flatten(),cthetas_i.flatten(),  c=cmap(my_norm(int_i.flatten())))
+                    # ax.scatter(xrs_i.flatten(), yrs_i.flatten(),np.log10(int_i.flatten()),  c=cmap(my_norm(int_i.flatten())))
+                    # # ax.scatter(xrs_i.flatten(), yrs_i.flatten(),cphis_i.flatten(),  c=cmap(my_norm(int_i.flatten())))
                     # ax.set_xlabel('X Label')
                     # ax.set_ylabel('Y Label')
                     # ax.set_zlabel('I Label')
+                    # ax.set_zlim(np.log10(int_i.flatten()).max()*1.-2,np.log10(int_i.flatten()).max())
                     # plt.show()
 
                 # Problem: changing nlayers changes the image/image, Fnu per pixel; Solution:
-                if renormalize:
+                if (renormalize and normtype=="pw"):
                     print("Renormalizing ejecta skymap (shell by shell separately)")
                     fnus_tot = np.zeros_like(self.get_skymap_times())
                     for i, i_ish in enumerate(i_shells):
@@ -1742,7 +1777,27 @@ class Ejecta(Base):
                             print("\tFnu/mas^2 = {:.2e} mJy/mas^2".format(dfnu))
                         all_zz[i] = (all_fluxes_arr / all_fluxes_arr.max()) * dfnu
                         fnus_tot = fnus_tot + fnus
-
+                elif (renormalize and normtype=="a"):
+                    print("Renormalizing ejecta skymap (shell by shell separately)")
+                    fnus_tot = np.zeros_like(self.get_skymap_times())
+                    for i, i_ish in enumerate(i_shells):
+                        fnus = self.get_skymap_totfluxes(freq=freq, shell=i_ish)
+                        fnu = fnus[find_nearest_index(self.get_skymap_times(), time)]
+                        all_fluxes_arr = np.array(all_zz[i])
+                        delta_x = np.array(all_xrs[i]).max() - np.array(all_xrs[i]).min()
+                        delta_y = np.array(all_yrs[i]).max() - np.array(all_yrs[i]).min()
+                        dfnu = fnu;#/ (delta_x * delta_y)
+                        if verbose:
+                            print("SHELL {}".format(i_ish))
+                            print("\tfnu = {:.2e} ".format(fnu))
+                            print("\tall_x = [{:.2e}, {:.2e}]".format(np.array(all_xrs).min(), np.array(all_xrs).max()))
+                            print("\tall_y = [{:.2e}, {:.2e}]".format(np.array(all_yrs).min(), np.array(all_yrs).max()))
+                            print("\tDelta_x = {:.2f}, Delta_y = {:.2f}]".format(delta_x, delta_y))
+                            print("\tFnu/mas^2 = {:.2e} mJy/mas^2".format(dfnu))
+                        all_zz[i] = (all_fluxes_arr / all_fluxes_arr.max()) * dfnu
+                        fnus_tot = fnus_tot + fnus
+                elif (renormalize):
+                    raise KeyError(f"norm type {normtype} is not recognized")
                 return (all_xrs, all_yrs, all_zz)
 
                 # assess what is the combined grid extend (for all images)

package/src/PyBlastAfterglowMag/skymap_tools.py
@@ -1433,28 +1433,28 @@ def _plot_skymap_with_hists(ax_main, ax_histx, ax_histy, pb, tmp,
     # ax.axvline(x=x2, color='gray', linestyle='dotted')
     # ax.axvline(x=xcs_m, color='gray', linestyle='solid')
     if ("cm" in tmp.keys()) and len(tmp["cm"].keys()) > 0:
-        ax_main.plot(xc, yc, **tmp["cm"])
-        ax_histx.axvline(x=xc, color=tmp["cm"]["color"], linestyle='dashed')
-        ax_histy.axhline(y=yc, color=tmp["cm"]["color"], linestyle='dashed')
+        if (not (ax_main is None)): ax_main.plot(xc, yc, **tmp["cm"])
+        if (not (ax_histx is None)): ax_histx.axvline(x=xc, color=tmp["cm"]["color"], linestyle='dashed')
+        if (not (ax_histy is None)): ax_histy.axhline(y=yc, color=tmp["cm"]["color"], linestyle='dashed')
     if ("ysize" in tmp.keys()) and len(tmp["ysize"].keys()) > 0:
         if (len(tmp["smooth"].keys()) > 0):
             i_zz_y = smooth_interpolated_skymap_with_gaussian_kernel(i_zz=i_zz_y, type=tmp["smooth"]["type"],
                                                                         sigma=tmp["smooth"]["sigma"])
         y1, y2 = get_skymap_fwhm(grid_y, i_zz_y, yc)
-        ax_main.errorbar([xc, xc], [y1, y2], xerr=[int_x.max() / 10, int_x.max() / 10], **tmp["ysize"])
-        ax_histy.plot(i_zz_y * 1e3, grid_y, lw=1., ls='-', drawstyle='steps', color=tmp["ysize"]["color"])
-        ax_histy.axhline(y=y2, color=tmp["ysize"]["color"], linestyle='dotted')
-        ax_histy.axhline(y=y1, color=tmp["ysize"]["color"], linestyle='dotted')
+        if (not (ax_main is None)): ax_main.errorbar([xc, xc], [y1, y2], xerr=[int_x.max() / 10, int_x.max() / 10], **tmp["ysize"])
+        if (not (ax_histy is None)): ax_histy.plot(i_zz_y * 1e3, grid_y, lw=1., ls='-', drawstyle='steps', color=tmp["ysize"]["color"])
+        if (not (ax_histy is None)): ax_histy.axhline(y=y2, color=tmp["ysize"]["color"], linestyle='dotted')
+        if (not (ax_histy is None)): ax_histy.axhline(y=y1, color=tmp["ysize"]["color"], linestyle='dotted')
     if ("xsize" in tmp.keys()) and len(tmp["xsize"].keys()) > 0:
         if (len(tmp["smooth"].keys()) > 0):
             i_zz_x = smooth_interpolated_skymap_with_gaussian_kernel(i_zz=i_zz_x, type=tmp["smooth"]["type"],
                                                                         sigma=tmp["smooth"]["sigma"])
         x1, x2 = get_skymap_fwhm(grid_x, i_zz_x, xc)
-        ax_main.errorbar([x1, x2], [yc, yc], yerr=[int_y.max() / 10, int_y.max() / 10], **tmp["xsize"])
-        ax_histx.plot(grid_x, i_zz_x * 1e3, lw=1., ls='-', drawstyle='steps',
+        if (not (ax_main is None)): ax_main.errorbar([x1, x2], [yc, yc], yerr=[int_y.max() / 10, int_y.max() / 10], **tmp["xsize"])
+        if (not (ax_histx is None)): ax_histx.plot(grid_x, i_zz_x * 1e3, lw=1., ls='-', drawstyle='steps',
                       color=tmp["xsize"]["color"])  # , color='blue')
-        ax_histx.axvline(x=x2, color=tmp["xsize"]["color"], linestyle='dotted')
-        ax_histx.axvline(x=x1, color=tmp["xsize"]["color"], linestyle='dotted')
+        if (not (ax_histx is None)): ax_histx.axvline(x=x2, color=tmp["xsize"]["color"], linestyle='dotted')
+        if (not (ax_histx is None)): ax_histx.axvline(x=x1, color=tmp["xsize"]["color"], linestyle='dotted')
     # --------------------
     # if len(tmp["pcolormesh"].keys()) > 0:
     #     # levels = np.geomspace(int_ration[~np.isnan(int_ration)].min(), int_ration[~np.isnan(int_ration)].max(), 50)